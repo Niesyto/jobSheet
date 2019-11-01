4ec2c6bf7deb2391c8df6445a660d2438163fbf7
niesyto <jarekniesyto@gmail.com>
2019-11-01 21:40:54 +0100

    Menu

---
+++ jobsheet/src/MenuDrawer.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import PersonIcon from '@material-ui/icons/Person';
+import DescriptionIcon from '@material-ui/icons/Description';
+
+
+const icons = [<PersonIcon/>,<DescriptionIcon/>];
+const menuList = ["Workers", "Projects"];
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles(theme => ({
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0
+  },
+  drawerPaper: {
+    width: drawerWidth
+  },
+  toolbar: theme.mixins.toolbar
+}));
+  
+
+export default function MenuDrawer(props){
+    const classes = useStyles();
+
+    function handleClick(index)
+    {
+      props.setSelectedOption(menuList[index]);
+    }
+
+    return(
+        <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper
+        }}
+      >
+        <div className={classes.toolbar} />
+        <List>
+          {menuList.map((text, index) => (
+            <ListItem button key={text} onClick={handleClick.bind(this, index)}>
+                <ListItemIcon >{icons[index]}</ListItemIcon>
+                <ListItemText primary={text} />
+          </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    )
+}
